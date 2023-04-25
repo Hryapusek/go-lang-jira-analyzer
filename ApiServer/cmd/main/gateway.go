e542d9b53b94e68669747b6ba7424e9cbb3e65d6
Foko <spike.calhun@yandex.ru>
2023-04-25 17:05:54 +0300

    Переделан gateway.go Проверен функционал проксирования

--- ApiServer/cmd/main/gateway.go
+++ ApiServer/cmd/main/gateway.go
@@ -3,21 +3,16 @@ package main
 import (
 	"ApiServer/internals/config"
 	"context"
+	_ "context"
 	"fmt"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
-	"strings"
-	"syscall"
+	_ "strings"
+	_ "syscall"
 	"time"
 )
-
-type route struct {
-	prefix  string
-	target  *url.URL
-	timeout time.Duration // время ожидания
-}
 
 func main() {
 	cfg := config.LoadGatewayConfig("configs/server.yaml")
@@ -42,55 +37,35 @@ func main() {
 
 	gatewayAddress := fmt.Sprintf("%s:%d", cfg.GatewayHost, cfg.GatewayPort)
 
-	// Разрешенные маршруты
-	routes := []route{
-		{cfg.AnalyticsAPIPrefix, analyticsTarget, time.Duration(cfg.AnalyticsTimeout) * time.Second},
-		{cfg.ResourceAPIPrefix, resourceTarget, time.Duration(cfg.ResourceTimeout) * time.Second},
-		{cfg.ConnectorAPIPrefix, connectorTarget, 0}, // микросервис connector без лимита
-	}
+	gatewayMux := http.NewServeMux()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc(cfg.GatewayAPIPrefix, func(w http.ResponseWriter, r *http.Request) {
-		for _, rt := range routes {
-			if strings.HasPrefix(r.URL.Path, rt.prefix) {
-				proxy := httputil.NewSingleHostReverseProxy(rt.target)
+	gatewayMux.HandleFunc(cfg.GatewayAPIPrefix+cfg.AnalyticsAPIPrefix, func(w http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(cfg.AnalyticsTimeout))
+		defer cancel()
 
-				if rt.timeout > 0 {
-					ctx, cancel := context.WithTimeout(r.Context(), rt.timeout)
-					defer cancel()
-					r = r.WithContext(ctx)
+		r = r.WithContext(ctx)
 
-					done := make(chan struct{})
-					go func() {
-						proxy.ServeHTTP(w, r)
-						close(done)
-					}()
-
-					select {
-					case <-done:
-					case <-ctx.Done():
-						w.WriteHeader(http.StatusGatewayTimeout)
-						_, err := fmt.Fprint(w, "Gateway Timeout")
-						if err != nil {
-							return
-						}
-					}
-				} else {
-					proxy.ServeHTTP(w, r)
-				}
-				return
-			}
-		}
-		http.Error(w, "Not found", http.StatusNotFound)
+		proxy := httputil.NewSingleHostReverseProxy(analyticsTarget)
+		proxy.ServeHTTP(w, r)
 	})
 
-	stop := make(chan os.Signal, 1)
+	gatewayMux.HandleFunc(cfg.GatewayAPIPrefix+cfg.ResourceAPIPrefix, func(w http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(cfg.ResourceTimeout))
+		defer cancel() // нужно добавить логирование запросов + вывод истекших по timeout
 
-	fmt.Println("Starting reverse proxy server on ", gatewayAddress)
-	go func() {
-		if err := http.ListenAndServe(gatewayAddress, mux); err != nil {
-			fmt.Printf("ListenAndServe error: %v\n", err)
-			stop <- syscall.SIGTERM
-		}
-	}()
+		r = r.WithContext(ctx)
+
+		proxy := httputil.NewSingleHostReverseProxy(resourceTarget)
+		proxy.ServeHTTP(w, r)
+	})
+
+	gatewayMux.HandleFunc(cfg.GatewayAPIPrefix+cfg.ConnectorAPIPrefix, func(w http.ResponseWriter, r *http.Request) {
+		proxy := httputil.NewSingleHostReverseProxy(connectorTarget)
+		proxy.ServeHTTP(w, r)
+	})
+
+	err = http.ListenAndServe(gatewayAddress, gatewayMux)
+	if err != nil {
+		return
+	}
 }
