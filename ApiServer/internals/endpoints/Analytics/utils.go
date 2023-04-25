ebe2a1be4f080665936f5eb39bb9ccfbbf23f7df
Foko <spike.calhun@yandex.ru>
2023-04-25 22:32:10 +0300

    пытаюсь через боль и слезы разобраться со схемой postgresql

---
+++ ApiServer/internals/endpoints/Analytics/utils.go
@@ -0,0 +1,10 @@
+package endpoints
+
+// TODO: заменить на конфигурационный файл, загружался бы вместе с analytics server
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "your-password"
+	dbname   = ""
+)
